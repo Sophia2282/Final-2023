f8950b58693efaa350ec82618dc27d1f7194fbe0
Sophia2282 <103472846+Sophia2282@users.noreply.github.com>
2023-04-01 00:37:13 -0400

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,11 @@
 package frc.robot;
 
 import frc.robot.Constants.ControllerPorts;
-
+import frc.robot.Constants.Speeds;
 import frc.robot.commands.ArcadeDrive;
-import frc.robot.commands.DriveTime;
+//import frc.robot.commands.AutoBalance;
+//import frc.robot.commands.BalanceCalc;
+// import frc.robot.commands.DriveTime;
 import frc.robot.commands.ElevatorUp;
 import frc.robot.commands.IntakeObject;
 import frc.robot.commands.LiftArm;
@@ -15,7 +17,6 @@ import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Elevator;
-import frc.robot.Constants.Speeds;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -32,74 +33,80 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private DriveTrain autoDriveTrain = new DriveTrain();
-  private double time = 15;
-  private final Intake m_intake = new Intake();
-  private final Arm m_arm = new Arm();
-  private final Elevator m_elevator = new Elevator();
-  private final DriveTrain m_drive = new DriveTrain();
+        // The robot's subsystems and commands are defined here...
+        private final Intake m_intake = new Intake();
+        private final Arm m_arm = new Arm();
+        private final Elevator m_elevator = new Elevator();
+        private final DriveTrain m_drive = new DriveTrain();
+        // private final BalanceCalc m_balancecalc = new BalanceCalc();
 
-  public XboxController driverController = new XboxController(ControllerPorts.kDriverControllerPort);
-  public Joystick subControllerJoystick = new Joystick(ControllerPorts.kJoystickPort);
+        public XboxController driverController = new XboxController(ControllerPorts.kDriverControllerPort);
+        public Joystick subControllerJoystick = new Joystick(ControllerPorts.kJoystickPort);
 
-  public RobotContainer() {
-    m_arm.setDefaultCommand(
-        new LiftArm(m_arm, () -> subControllerJoystick.getRawButton(6),
-            () -> subControllerJoystick.getRawButton(4)));
+        public RobotContainer() {
+                m_arm.setDefaultCommand(
+                                new LiftArm(m_arm, () -> subControllerJoystick.getRawButton(6),
+                                                () -> subControllerJoystick.getRawButton(4)));
 
-    m_intake.setDefaultCommand(
-        new IntakeObject(m_intake, () -> subControllerJoystick.getRawButton(3),
-            () -> subControllerJoystick.getRawButton(5), () -> subControllerJoystick.getRawButton(2)));
+                m_intake.setDefaultCommand(
+                                new IntakeObject(m_intake, () -> subControllerJoystick.getRawButton(3),
+                                                () -> subControllerJoystick.getRawButton(5),
+                                                () -> subControllerJoystick.getRawButton(2)));
 
-    // m_elevator.setDefaultCommand(
-    // new ElevatorUp(m_elevator, () -> subControllerJoystick.getRawAxis(1) * 0.5));
+                m_elevator.setDefaultCommand(
+                                new ElevatorUp(m_elevator, () -> subControllerJoystick.getRawButton(10),
+                                                () -> subControllerJoystick.getRawButton(8)));
 
-    m_elevator.setDefaultCommand(
-        new ElevatorUp(m_elevator, () -> subControllerJoystick.getRawButton(10),
-            () -> subControllerJoystick.getRawButton(8)));
+                configureBindings();
+                m_drive.setDefaultCommand(getarcadeDriveCommand());
+        }
 
-    configureBindings();
-    m_drive.setDefaultCommand(getarcadeDriveCommand());
+        private void configureBindings() {
 
-    // m_arm.setDefaultCommand(new Lift(m_arm, 0));
+                // Schedule `exampleMethodComfmand` when the Xbox controller's B button is
+                // pressed,
+                // cancelling on release.
+                // driverController.getRawButton(1).whileTrue(arcadeDrive.());
+        }
 
-    // m_intake.setDefaultCommand(new IntakeObject(m_intake, 0));
-    // m_elevator.setDefaultCommand(new ElevatorUp(m_elevator, 0));
-  }
+        public Command getAutonomousCommand() {
+                // return new InstantCommand();
+                return new SequentialCommandGroup(
+                                new StartEndCommand(
+                                                () -> m_arm.setArmSpeed(Speeds.ARM_SPEED),
+                                                () -> m_arm.armstop(), m_arm).withTimeout(1.5),
+                                new StartEndCommand(
+                                                () -> m_elevator.setElevatorSpeed(Speeds.ELEVATOR_SPEED),
+                                                () -> m_elevator.ElevatorStop(), m_elevator).withTimeout(1.2),
+                                new StartEndCommand(
+                                                () -> m_intake.setIntakeSpeed(Speeds.INTAKE_SPEED * -1),
+                                                () -> m_intake.stopIntake(), m_intake).withTimeout(1),
+                                new StartEndCommand(
+                                                () -> m_arm.setArmSpeed(Speeds.ARM_SPEED * -1),
+                                                () -> m_arm.armstop(), m_arm).withTimeout(1.5),
+                                new StartEndCommand(
+                                                () -> m_elevator.setElevatorSpeed(Speeds.ELEVATOR_SPEED * -1),
+                                                () -> m_elevator.ElevatorStop(), m_elevator).withTimeout(1),
+                                new StartEndCommand(
+                                                () -> m_drive.setPower(Speeds.DRIVE_SPEED * -1),
+                                                () 
+                                                () -> m_drive.stop(), m_drive).withTimeout(2));
+                // new StartEndCommand(
+                // try during practice match () -> m_drive.setPower(Speeds.DRIVE_SPEED * -1),
+                // () -> m_drive.stop(), m_drive).withTimeout(1.8));
+                // new StartEndCommand(
+                // () -> m_arm.setArmSpeed(Speeds.ARM_SPEED),
+                // () -> m_arm.armstop(), m_arm).withTimeout(1.25),
 
-  private void configureBindings() {
+        }
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
-    // pressed,
-    // cancelling on release.
-    // driverController.getRawButton(1).whileTrue(arcadeDrive.());
-  }
+        // m_drive.setDefaultCommand(
+        // new slowMode(m_drive,() -> driverController.getRawButton(1));
 
-  public Command getAutonomousCommand() {
-    // return new InstantCommand();
-    return new SequentialCommandGroup(
-
-        new StartEndCommand(
-            () -> m_arm.setArmSpeed(Speeds.ARM_SPEED),
-            () -> m_arm.armstop(), m_arm).withTimeout(1),
-        new StartEndCommand(
-            () -> m_elevator.setElevatorSpeed(Speeds.ELEVATOR_SPEED),
-            () -> m_elevator.ElevatorStop(), m_elevator).withTimeout(1),
-        new StartEndCommand(
-            () -> m_intake.setIntakeSpeed(Speeds.INTAKE_SPEED * -1),
-            () -> m_intake.stopIntake(), m_intake).withTimeout(3),
-        new StartEndCommand(
-            () -> m_drive.setPower(Speeds.DRIVE_SPEED * -1),
-            () -> m_drive.stop(), m_drive).withTimeout(3));
-  }
-
-  // m_drive.setDefaultCommand(
-  // new slowMode(m_drive,() -> driverController.getRawButton(1));
-
-  public Command getarcadeDriveCommand() {
-    return new ArcadeDrive(
-        m_drive, () -> -driverController.getRawAxis(1), () -> -driverController.getRawAxis(4),
-        () -> driverController.getRawButton(1)); // change into left bumper?
-  }
+        public Command getarcadeDriveCommand() {
+                return new ArcadeDrive(
+                                m_drive,
+                                () -> (-driverController.getRawAxis(1) * (driverController.getAButton() ? 0.7 : 1)),
+                                () -> (-driverController.getRawAxis(4) * (driverController.getAButton() ? 0.7 : 1)));
+        }
 }
